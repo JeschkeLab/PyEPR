5a4719031df21f94c1134986a85091137c977718
Hugo Karas <hkaras@ethz.ch>
2025-06-26 16:09:12 +0200

    Updated version detection

--- pyepr/_version.py
+++ pyepr/_version.py
@@ -1,12 +1,14 @@
 import toml
 import importlib.util
+import importlib.metadata
 import os.path
 MODULE_DIR = importlib.util.find_spec('pyepr').submodule_search_locations[0]
 
-with open(os.path.join(MODULE_DIR,"../pyproject.toml"), "r") as f:
-    config = toml.load(f)
-    __version__ = config["tool"]["poetry"]["version"]
+# with open(os.path.join(MODULE_DIR,"../pyproject.toml"), "r") as f:
+#     config = toml.load(f)
+#     __version__ = config["tool"]["poetry"]["version"]
 
+__version__ = importlib.metadata.version('PyEPR')
 __copyright__ = "2021-2024, Hugo Karas, Gunnar Jeschke, Stefan Stoll"
 
 try: 
