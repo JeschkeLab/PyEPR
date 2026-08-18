241a78956d237e5e5a566da959b2a906c5aa4ffe
Hugo Karas <hkaras@ethz.ch>
2026-08-18 11:07:15 +0200

    Improved pulse tuning

--- pyepr/classes.py
+++ pyepr/classes.py
@@ -12,7 +12,10 @@ from pyepr.utils import autoEPRDecoder, round_step
 from pathlib import Path
 import logging
 from pyepr.config import get_waveform_precision
+import pyepr.pulses as pulses
 import yaml
+import warnings
+from pyepr.sequences import HahnEchoSequence
 
 
 # =============================================================================
@@ -49,6 +52,8 @@ class Interface:
             self.amp_nonlinearity = self.config["Spectrometer"]["Bridge"].get('Amplifier Non-Linearity',None)
         else:
             self.amp_nonlinearity = None
+
+        self.AWG=True
         pass
 
     def connect(self) -> None:
@@ -212,6 +217,308 @@ class Interface:
         
         self.terminate()
         pass
+
+    def tune_RectPulse(self,freq, B, reptime, tp, offset_freq, shots=200, verbosity=0,**kwargs):
+        """
+        Tunes a pair of equal power pi/2, pi rectangular pulses though an amplitude sweep. 
+        
+        Parameters
+        ----------
+        freq : float
+            The central frequency of the experiment in GHz.
+        B : float
+            The magnetic field in Gauss.
+        reptime : float
+            The repetition time in us.
+        tp : float
+            The length of the pi/2 pulse in ns. The pi pulse will be half this length. 
+            This will be rounded to the nearest 2*waveform_precision to ensure that the pi pulse is of the correct length.
+        offset_freq : float
+            The offset frequency of the rectangular pulse in GHz. This is used to shift the pulse away from the central frequency of the experiment.
+        shots : int, optional
+            The number of shots to be used for the frequency sweep, by default 200.
+        verbosity : int, optional
+            The verbosity level, by default 0. If > 0, the scale values will be printed.
+        **kwargs : dict
+            - step : float, optional
+                The step size for the amplitude parameter, by default 0.01.
+            - dim : int, optional
+                The dimension of the amplitude parameter, by default 50.
+            - tau : float, optional
+                The delay between the pi/2 and pi pulses in ns, by default 500.
+        ** Interface specific launch commands
+
+        Returns
+        -------
+        p90 : pulses.RectPulse
+            The tuned pi/2 pulse.
+        p180 : pulses.RectPulse
+            The tuned pi pulse.
+        """
+        step = kwargs.pop('step', 0.01)
+        dim = kwargs.pop('dim', 50)
+        start = kwargs.pop('start', 0.0)
+        tau = kwargs.pop('tau', 500)
+
+        amp = Parameter(name="scale", value=start, step=step, dim=dim)
+        p90 = pulses.RectPulse(tp=tp,freq=offset_freq,flipangle=np.pi/2,scale=amp)
+        p180 = pulses.RectPulse(tp=tp,freq=offset_freq,flipangle=np.pi,scale=amp)
+        RectEchoTuneSeq =HahnEchoSequence(
+                    B=B, freq=freq, reptime=reptime, averages=1, shots=shots, tau=tau,
+                    pi2_pulse=p90, pi_pulse=p180,
+                    name='RectPulse Echo Tune Sequence')
+
+        RectEchoTuneSeq.evolution([amp])
+
+        self.launch(RectEchoTuneSeq, 
+                    savename=f"RectEchoTune",
+                    **kwargs)
+        time.sleep(5)
+        while self.isrunning():
+            time.sleep(1)
+
+        dataset = self.acquire_dataset(filter_type='cheby',filter_width=p90.bandwidth.value*1e3)
+
+        if np.any(dataset.data == 0):
+            warnings.warn("Zero values found in dataset. This may indicate an error in acquisition.")
+            time.sleep(5)
+            while self.isrunning():
+                time.sleep(2)
+            dataset = self.acquire_dataset(downconvert=True,reduce=True,filter_type='boxcar',filter_width=250)
+
+        dataset = dataset.epr.correctphase
+        data = np.abs(dataset.data)
+        scale = np.around(dataset.pulse0_scale[data.argmax()].data,2)
+        if verbosity > 0:
+            print(f"Scale Value: {scale}")
+        if scale > 0.95:
+            raise RuntimeError("Not enough power avaliable.")
+
+        if scale == 0:
+            warnings.warn("Pulse tuned with a scale of zero!")
+            print("Pulse tuned with a scale of zero!")
+        p90 = p90.copy(scale=scale)
+        p180 = p180.copy(scale=scale)
+
+        return p90, p180
+
+
+
+    def tune_GaussianPulse(self,freq, B, reptime, tp, offset_freq, shots=200, verbosity=0,**kwargs):
+        """
+        Tunes a pair of equal power pi/2, pi Gaussian pulses though an amplitude sweep. 
+        
+        Parameters
+        ----------
+        freq : float
+            The central frequency of the experiment in GHz.
+        B : float
+            The magnetic field in Gauss.
+        reptime : float
+            The repetition time in us.
+        tp : float
+            The length of the pi/2 pulse in ns. The pi pulse will be half this length. 
+            This will be rounded to the nearest 2*waveform_precision to ensure that the pi pulse is of the correct length.
+        offset_freq : float
+            The offset frequency of the Gaussian pulse in GHz. This is used to shift the pulse away from the central frequency of the experiment.
+        shots : int, optional
+            The number of shots to be used for the frequency sweep, by default 200.
+        verbosity : int, optional
+            The verbosity level, by default 0. If > 0, the scale values will be printed.
+        **kwargs : dict
+            - step : float, optional
+                The step size for the amplitude parameter, by default 0.01.
+            - dim : int, optional
+                The dimension of the amplitude parameter, by default 50.
+            - tau : float, optional
+                The delay between the pi/2 and pi pulses in ns, by default 500.
+        ** Interface specific launch commands
+
+        Returns
+        -------
+        p90 : pulses.GaussianPulse
+            The tuned pi/2 pulse.
+        p180 : pulses.GaussianPulse
+            The tuned pi pulse.
+
+        """
+
+        if not self.AWG:
+            raise RuntimeError("Only AWG based spectrometers can tune Gaussian pulses.")
+
+        step = kwargs.pop('step', 0.01)
+        dim = kwargs.pop('dim', 50)
+        start = kwargs.pop('start', 0.0)
+        tau = kwargs.pop('tau', 500)
+
+        amp = Parameter(name="scale", value=start, step=step, dim=dim)
+        p90 = pulses.GaussianPulse(tp=tp,freq=offset_freq,flipangle=np.pi/2,scale=amp)
+        p180 = pulses.GaussianPulse(tp=tp,freq=offset_freq,flipangle=np.pi,scale=amp)
+        GaussianEchoTuneSeq =HahnEchoSequence(
+                    B=B, freq=freq, reptime=reptime, averages=1, shots=shots, tau=tau,
+                    pi2_pulse=p90, pi_pulse=p180,
+                    name='GaussianPulse Echo Tune Sequence')
+
+        GaussianEchoTuneSeq.evolution([amp])
+
+        self.launch(GaussianEchoTuneSeq, 
+                    savename=f"GaussEchoTune",
+                    **kwargs)
+        time.sleep(5)
+        while self.isrunning():
+            time.sleep(1)
+
+        dataset = self.acquire_dataset(filter_type='cheby',filter_width=p90.bandwidth.value*1e3)
+
+        if np.any(dataset.data == 0):
+            warnings.warn("Zero values found in dataset. This may indicate an error in acquisition.")
+            time.sleep(5)
+            while self.isrunning():
+                time.sleep(2)
+            dataset = self.acquire_dataset(downconvert=True,reduce=True,filter_type='boxcar',filter_width=250)
+
+        dataset = dataset.epr.correctphase
+        data = np.abs(dataset.data)
+        scale = np.around(dataset.pulse0_scale[data.argmax()].data,2)
+        if verbosity > 0:
+            print(f"Scale Value: {scale}")
+        if scale > 0.95:
+            raise RuntimeError("Not enough power avaliable.")
+
+        if scale == 0:
+            warnings.warn("Pulse tuned with a scale of zero!")
+            print("Pulse tuned with a scale of zero!")
+        p90 = p90.copy(scale=scale)
+        p180 = p180.copy(scale=scale)
+
+        return p90, p180
+
+    
+    def tune_KBBPulse(self,*, freq, B, reptime, tp, init_freq, final_freq, shots=200, verbosity=0,**kwargs):
+        """
+        Tunes a Kutz-Boehlen-Bodenhausen linear chirp echo. This is a three step optimisation:
+
+        1. Both pulses amplitudes are swept with a 1:2 ratio.
+        2. Pulse 0 is fixed using the optimal amplitude from step 1 and pulse 1 is swept.
+        3. Pulse 1 is fixed using the optimal amplitude from step 2 and pulse 0 is swept.
+
+        Parameters
+        ----------
+        freq : float
+            The central frequency of the experiment in GHz.
+        B : float
+            The magnetic field in Gauss.
+        reptime : float
+            The repetition time in us.
+        tp : float
+            The length of the pi/2 pulse in ns. The pi pulse will be half this length. 
+            This will be rounded to the nearest 2*waveform_precision to ensure that the pi pulse is of the correct length.
+        init_freq : float
+            The initial frequency of the frequency sweep in GHz.
+        final_freq : float
+            The final frequency of the frequency sweep in GHz.
+        shots : int, optional
+            The number of shots to be used for the frequency sweep, by default 200.
+        verbosity : int, optional
+            The verbosity level, by default 0. If > 0, the scale values will be printed.
+        **kwargs : dict
+            - step : float, optional
+                The step size for the amplitude parameter, by default 0.01.
+            - dim : int, optional
+                The dimension of the amplitude parameter, by default 50.
+            - tau : float, optional
+                The delay between the pi/2 and pi pulses in ns, by default 500.
+        ** Interface specific launch commands
+
+        Returns
+        -------
+        p90 : pulses.ChirpPulse
+            The tuned pi/2 pulse.
+        p180 : pulses.ChirpPulse
+            The tuned pi pulse.
+        """
+        if not self.AWG:
+            raise RuntimeError("Only AWG based spectrometers can tune KBB pulses.")
+
+        waveform_precision = get_waveform_precision()
+        tp = round_step(tp,waveform_precision*2) # Round to nearest 2*waveform_precision to ensure that the pi pulse is an integer multiple of waveform_precision
+
+        step = kwargs.pop('step', 0.01)
+        dim = kwargs.pop('dim', 50)
+        start = kwargs.pop('start', 0.0)
+        amp = Parameter(name="Amp", value=start, step=step, dim=dim)
+
+        p90 = pulses.ChirpPulse(
+            tp=tp,init_freq=init_freq,final_freq=final_freq,flipangle=np.pi/2)
+        p180 = pulses.ChirpPulse(
+            tp=tp*0.5,init_freq=init_freq,final_freq=final_freq,flipangle=np.pi)
+
+
+        tau = kwargs.pop('tau', 500)
+
+        ChirpEchoTuneSeq = HahnEchoSequence(
+            freq=freq,B=B, reptime=reptime, shots=shots, averages=1, pi2_pulse=p90, 
+            pi_pulse=p180, tau=tau)
+        ChirpEchoTuneSeq.evolution([amp])
+
+
+        self.launch(ChirpEchoTuneSeq, 
+                    savename=f"chirpechotune_step1",
+                    **kwargs)
+        time.sleep(5)
+        while self.isrunning():
+            time.sleep(1)
+
+        dataset = self.acquire_dataset(filter_type='cheby',filter_width=p90.bandwidth.value*1e3)
+
+        scale = np.around(dataset.pulse0_scale[np.abs(dataset.epr.correctphase).argmax()].data,2)
+
+        if verbosity > 0:
+            print(f"Initial Scale Value: {scale}")
+
+        ChirpEchoTuneSeq.pulses[0].scale = Parameter('scale',value=scale)
+        ChirpEchoTuneSeq.pulses[1].scale = amp*2
+        ChirpEchoTuneSeq.evolution([amp])
+
+        self.launch(ChirpEchoTuneSeq, 
+                            savename=f"chirpechotune_step2",
+                            **kwargs)
+        
+        while self.isrunning():
+            time.sleep(1)
+        
+        dataset = self.acquire_dataset(filter_type='cheby',filter_width=p90.bandwidth.value*1e3)
+        
+        scale1 = np.around(dataset.pulse1_scale[np.abs(dataset.epr.correctphase).argmax()].data,2)
+
+        if verbosity > 0:
+            print(f"Improved pulse1 scale: {scale1}")
+
+        ChirpEchoTuneSeq.pulses[0].scale = amp
+        ChirpEchoTuneSeq.pulses[1].scale = Parameter('scale',value=scale)
+        ChirpEchoTuneSeq.evolution([amp])
+
+        self.launch(ChirpEchoTuneSeq, 
+                                    savename=f"chirpechotune_step3",
+                                    **kwargs)
+
+        while self.isrunning():
+                    time.sleep(1)
+                
+        dataset = self.acquire_dataset(filter_type='cheby',filter_width=p90.bandwidth.value*1e3)
+        
+        scale0 = np.around(dataset.pulse0_scale[np.abs(dataset.epr.correctphase).argmax()].data,2)
+
+        if verbosity > 0:
+            print(f"Improved pulse0 scale: {scale0}")
+
+        p90 = pulses.ChirpPulse(
+            tp=tp,init_freq=init_freq,final_freq=final_freq,flipangle=np.pi/2,scale=scale0)
+        p180 = pulses.ChirpPulse(
+            tp=tp*0.5,init_freq=init_freq,final_freq=final_freq,flipangle=np.pi,scale=scale1)
+
+        return p90, p180
+
 
 # =============================================================================
 
